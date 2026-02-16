762a0b40b9e902bba5ee32569222817edeec4d1e
Titamah <titamah@icloud.com>
2026-02-16 01:31:06 -0500

    Adding selection handling skeleton for future implementation.

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ def main():
     print("Welcome to Audio Memos. Here you can create and manage your audio notes.")
     if not utils.is_empty():
         print("Here are your saved memos:")
-        utils.print_library()
         ans = utils.select_item()
-        print(ans.name)
+        utils.handle_selection(ans)
         print("Good job!!!")
     else:
         print("You don't have any memos saved! Record or upload one to get started.")
--- utils/file_manager.py
+++ utils/file_manager.py
@@ -25,17 +25,54 @@ def is_empty():
 
 def get_selection():
     while True:
+        print_library()
         i =  input("Enter the name of the item to select: ")
         if i in library:
             return library[i]
         print("This item doesn't exist! Try again.")
-        print_library()
 
 def select_item():
     item = get_selection()
     if item.is_dir():
         change_level(item)
-        print_library()
         return select_item()
     elif item.is_file():
         return(item)
+    
+def edit_file(file):
+    print("Pretend you're editing this file!")
+    handle_selection(file)
+
+    
+def play_file(file):
+    print("Pretend you're listening to this file.")
+    handle_selection(file)
+
+def delete_file(file):
+    while True:
+        i = input(f"Are you sure you want to delete {file.name}? Enter Y for yes or N for no: ")
+        if i.capitalize() == "Y":
+            library.pop(file.name).unlink()
+            print("File deleted.")
+            return True
+        if i.capitalize() == "N":
+            print("Canceled. Returning to menu.")
+            handle_selection(file)
+        print("Hm, I didn't catch that. Try again.")
+
+def handle_selection(file):
+    if not file.is_file():
+        print("Hmm, this isn't file!")
+    else:
+        while True:
+            print(f"--- Currect selection: : {file.name} ---")
+            i = input("[E]dit, [D]elete, or [L]isten: ")
+            if i.capitalize() == "E":
+                edit_file(file)
+            elif i.capitalize() == "D":
+                if delete_file(file):
+                    select_item()
+            elif i.capitalize() == "L":
+                play_file(file)
+            print("Hm, I didn't catch that. Try again.")
+
