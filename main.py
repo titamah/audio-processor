bfc7b162153f50d29a4f98eaa0c143e14b0a6689
Titamah <titamah@icloud.com>
2026-02-15 23:39:09 -0500

    Changing File List -> Library Dict with recursive exploration

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import utils.file_manager as utils
 
+curr_dir = ""
+
 def main():
     print("Welcome to Audio Memos. Here you can create and manage your audio notes.")
-    if utils.has_files():
+    if not utils.is_empty():
         print("Here are your saved memos:")
-        utils.get_files()
+        utils.print_library()
+        ans = utils.select_item()
+        print(ans.name)
+        print("Good job!!!")
     else:
         print("You don't have any memos saved! Record or upload one to get started.")
 
--- utils/file_manager.py
+++ utils/file_manager.py
@@ -1,16 +1,41 @@
 from pathlib import Path
 
 p = Path('./library')
-files = list(p.glob('**/*'))
 
-files = [x.name for x in files if x.name != ".gitignore"]
+library = {
+    (f.name + "/" if f.is_dir() else f.name): f 
+    for f in p.glob('*') 
+    if f.name != ".gitignore"
+}
 
-def get_files():
-    for file in files:
-        print(file)
+def print_library():
+    for item in library.keys():
+        print(item)
 
-def has_files():
-    if len(files) == 0: 
-        return False 
-    else: 
-        return True
+def change_level(d):
+    global library 
+    library = {
+    (f.name + "/" if f.is_dir() else f.name): f 
+    for f in d.glob('*') 
+    if f.name != ".gitignore"
+}
+
+def is_empty():
+    return not bool(library)
+
+def get_selection():
+    while True:
+        i =  input("Enter the name of the item to select: ")
+        if i in library:
+            return library[i]
+        print("This item doesn't exist! Try again.")
+        print_library()
+
+def select_item():
+    item = get_selection()
+    if item.is_dir():
+        change_level(item)
+        print_library()
+        return select_item()
+    elif item.is_file():
+        return(item)
